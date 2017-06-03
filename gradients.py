0a981dba8b9b10a92263362c92320e506bd67839
nparthas <neilp1998@gmail.com>
2017-06-03 18:03:50 -0400

    finished version

--- gradients.py
+++ gradients.py
@@ -10,15 +10,15 @@ def read_format_data(filename):
     l_0 = {
         "as_received": 32.033333333333333,
         "annealed": 31.113333333333,
-        "cold_rolled": 19.4
+        "cold_rolled": 39.4
     }
     csa = {
-        "as_received": 1.15759 * 10**-5,
-        "cold_rolled": 7.9576 * 10**-6,
-        "annealed": 1.5616 * 10**-5
+        "as_received": 1.15759 * 10 ** -5,
+        "cold_rolled": 7.9576 * 10 ** -6,
+        "annealed": 1.5616 * 10 ** -5
     }
     points["Strain"] = [(x / l_0[name]) for x in points["Strain"]]
-    points["Stress"] = [(x * 9.80665)/csa[name] for x in points["Stress"]]
+    points["Stress"] = [(x * 9.80665) / csa[name] for x in points["Stress"]]
 
     return points
 
@@ -60,23 +60,40 @@ def plot_values(title_name, points, reg_values):
     plt.text(spacing_x * 0.04, spacing_y * 0.85,
              "Linear Fit Equation for Elastic Region: \ny ={0:.3f}E9x + {1:.3f}E6 Pa \nr = {2:.2f}"
              .format(
-                 (reg_values[0]/10**9),
-                 (reg_values[1]/10**6),
+                 (reg_values[0] / 10 ** 9),
+                 (reg_values[1] / 10 ** 6),
                  reg_values[2]))
 
-    plt.plot(points["Strain"][:7], f(points["Strain"][:7], reg_values[0], reg_values[1]), "k--",
+    plt.plot(points["Strain"][:reg_num], f(points["Strain"][:reg_num], reg_values[0], reg_values[1]), "k--",
              label="Linear Fit")
 
-    plt.plot(points["Strain"][:9], offset(points["Strain"][:9], reg_values[0], reg_values[1]), label="0.2% offset")
+    plt.plot(points["Strain"][:offset_num], offset(points["Strain"][:offset_num], reg_values[0], reg_values[1]), "b--",
+             label="0.2% offset")
 
     plt.legend(loc=4, borderaxespad=0.)
 
+    plt.annotate("Elastic Region", xy=(0.026, 9 * 10 ** 7), xytext=(0.04, 8 * 10 ** 7),
+                 arrowprops=dict(facecolor='black', shrink=0.1, width=2, headwidth=8))
+
+    plt.annotate("Yield \nStrength", xy=(0.075, 2.53 * 10 ** 8), xytext=(0.077, 1.8 * 10 ** 8),
+                 arrowprops=dict(facecolor='black', shrink=0.1, width=2, headwidth=8))
+
+    plt.annotate("Ultimate Tensile \nStrength", xy=(0.06, 2.66 * 10 ** 8), xytext=(0.07, 2.9 * 10 ** 8),
+                 arrowprops=dict(facecolor='black', shrink=0.1, width=2, headwidth=8))
+
+    plt.annotate("Fracture \nStress", xy=(0.0957, 2.17 * 10 ** 8), xytext=(0.091, 1.3 * 10 ** 8),
+                 arrowprops=dict(facecolor='black', shrink=0.1, width=2, headwidth=8))
     plt.show()
 
 
+annealed = [6, 9]  # reg_num, offset num
+as_received = [10, 14]
+cold_rolled = [7, 11]
+
+reg_num = annealed[0]
+offset_num = annealed[1]
 points = read_format_data("annealed.csv")
-reg_values = regression(points["Strain"][:6], points["Stress"][:6])
+reg_values = regression(points["Strain"][:reg_num], points["Stress"][:reg_num])
 plot_values("Annealed", points, reg_values)
 
-
-
+print(points['Stress'][-1])
